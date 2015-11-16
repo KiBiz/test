7158e5a87cbbfa494d59ba510c8805d76067967d
kibiz <kibiz1337@gmail.com>
2015-11-16 22:16:36 +0100

    Add branch naming convention

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 # Test and test and more tests, and more
+#
+# Three different branch names are commonly used in the open soure community
+
+# Bug fixes: fix/remove_stuff
+
+# Regular new functionality: topic/add_function_abc
+
+# Major new functionality: feature/add_huge_new_api
